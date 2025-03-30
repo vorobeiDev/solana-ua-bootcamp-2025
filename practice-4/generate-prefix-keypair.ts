91a268129347b0d7526f5e583a39520b207e022d
vorobeiDev <vorobei.dev@gmail.com>
2025-03-30 19:19:39 +0300

    Added key generator

---
+++ practice-4/generate-prefix-keypair.ts
@@ -0,0 +1,26 @@
+import { Keypair } from "@solana/web3.js";
+
+const TARGET_PREFIX = "vlad";
+
+function findVanityKey() {
+    let tries = 0;
+
+    while (true) {
+        const keypair = Keypair.generate();
+        const pubKeyBase58 = keypair.publicKey.toBase58();
+        tries++;
+
+        if (pubKeyBase58.toLowerCase().startsWith(TARGET_PREFIX)) {
+            console.log("Match found after", tries, "tries!");
+            console.log("Public Key:", pubKeyBase58);
+            console.log("Private Key (Base58):", keypair.secretKey);
+            return;
+        }
+
+        if (tries % 1000 === 0) {
+            console.log("Tried", tries, "keys so far...");
+        }
+    }
+}
+
+findVanityKey();
